e915a9e629ea5241e547726b232452febc62fd9c
tommi <tpagliari98@gmail.com>
2025-04-10 12:26:42 +0200

    afunctor instance for Parser

---
+++ 10_applicative_part1/AParser.hs
@@ -0,0 +1,64 @@
+module AParser where
+
+import Control.Applicative
+import Data.Char
+
+-- A parser for a value of type a is a function which takes a String
+-- represnting the input to be parsed, and succeeds or fails; if it
+-- succeeds, it returns the parsed value along with the remainder of
+-- the input.
+newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+
+-- For example, 'satisfy' takes a predicate on Char, and constructs a
+-- parser which succeeds only if it sees a Char that satisfies the
+-- predicate (which it then returns).  If it encounters a Char that
+-- does not satisfy the predicate (or an empty input), it fails.
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser f
+  where
+    f [] = Nothing    -- fail on the empty input (empty string)
+    f (x:xs)          -- check if char 'x' satisfies the predicate
+                      -- if so, return x along with the remainder
+                      -- of the input (that is, xs)
+        | p x       = Just (x, xs)
+        | otherwise = Nothing  -- otherwise, fail
+
+-- Using satisfy, we can define the parser 'char c' which expects to
+-- see exactly the character c, and fails otherwise.
+char :: Char -> Parser Char
+char c = satisfy (== c)
+
+{- For example:
+
+*Parser> runParser (satisfy isUpper) "ABC"
+Just ('A',"BC")
+*Parser> runParser (satisfy isUpper) "abc"
+Nothing
+*Parser> runParser (char 'x') "xyz"
+Just ('x',"yz")
+
+-}
+
+-- For convenience, we've also provided a parser for positive
+-- integers.
+posInt :: Parser Integer
+posInt = Parser f
+  where
+    f xs
+      | null ns   = Nothing
+      | otherwise = Just (read ns, rest)
+      where (ns, rest) = span isDigit xs
+
+------------------------------------------------------------
+-- Homework code goes below here
+------------------------------------------------------------
+
+-- Functor instance for Parser
+instance Functor Parser where
+  fmap :: (a -> b) -> Parser a -> Parser b
+  fmap h (Parser f) = Parser (first h . f)
+
+first :: (a -> b) -> Maybe (a,c) -> Maybe (b,c)
+first h = fmap h'
+  where h' (x, y) = (h x, y)
+
